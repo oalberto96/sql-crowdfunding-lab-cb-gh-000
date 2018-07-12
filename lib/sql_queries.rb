e7c99f14b47fa4b8c7f116b92cbf266e8b5f5516
Alberto Ochoa <ochoapublic@gmail.com>
2018-07-12 04:11:18 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -41,7 +41,13 @@ order by total_amount
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+"select projects.category, sum(pledges.amount)
+from pledges
+inner join projects
+on pledges.project_id = projects.id
+where projects.category = 'music'
+group by projects.category
+"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
