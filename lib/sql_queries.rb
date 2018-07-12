e4a9abbc1b2a3df199d274314c8b4e1069b2665d
Alberto Ochoa <ochoapublic@gmail.com>
2018-07-12 03:42:55 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,11 +7,10 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"select projects.title, pledges.amount from pledges
+"select projects.title, sum(pledges.amount) from pledges
 inner join projects
 on pledges.project_id = projects.id
-group by projects.title
-order by projects.title "
+group by projects.title"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
